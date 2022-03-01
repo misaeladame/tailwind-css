81a5b5554e4ef49c4c4a55f0bb660cfd60181657
Misael Adame <misael_adame@protonmail.com>
2022-03-01 00:57:13 -0600

    Grid de Tailwind

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,13 +5,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>TailWind</title>
-    <link rel="stylesheet" href="{{asset("css/app.css")}}">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
 <body>
-    <h1>Este es un h1</h1>
-    <h2>Este es un h2</h2>
-    <h3>Este es un h3</h3>
-    <h4>Este es un h4</h4>
-    <h5>Este es un h5</h5>
+    <div class="container">
+        {{-- <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4"> --}}
+        {{-- <div class="grid grid-cols-4 grid-rows-2 gap-4">
+            <div class="bg-blue-200">A</div>
+            <div class="bg-blue-300">B</div>
+            <div class="bg-blue-400 col-span-2 row-span-2">C</div>
+            <div class="bg-blue-500">D</div>
+            <div class="bg-blue-600">E</div>
+        </div> --}}
+        <div class="grid grid-flow-col grid-rows-3 grid-cols-4">
+            <div class="bg-blue-100">1</div>
+            <div class="bg-blue-200">2</div>
+            <div class="bg-blue-300">3</div>
+            <div class="bg-blue-400">4</div>
+            <div class="bg-blue-500">5</div>
+            <div class="bg-blue-600">6</div>
+            <div class="bg-blue-700">7</div>
+            <div class="bg-blue-800">8</div>
+            <div class="bg-blue-900">9</div>
+        </div>
+    </div>
 </body>
 </html>
