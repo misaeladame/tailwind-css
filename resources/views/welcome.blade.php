1e61efad85dbd190974da9d7e89d37348e4e3bb7
Misael Adame <misael_adame@protonmail.com>
2022-02-28 23:50:38 -0600

    Instalar Tailwind

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>TailWind</title>
+    <link rel="stylesheet" href="{{asset("css/app.css")}}">
+</head>
+<body>
+    <h1>Este es un h1</h1>
+    <h2>Este es un h2</h2>
+    <h3>Este es un h3</h3>
+    <h4>Este es un h4</h4>
+    <h5>Este es un h5</h5>
+</body>
+</html>
