07718b24291522f2686fc6f7903e72cb2ecb9359
Misael Adame <misael_adame@protonmail.com>
2022-03-01 12:33:12 -0600

    Tipografia

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>TailWind</title>
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link rel="stylesheet" href="{{url(mix('css/app.css'))}}">
 </head>
 <body>
     <div class="container">
@@ -17,6 +17,7 @@
             <div class="bg-blue-500">D</div>
             <div class="bg-blue-600">E</div>
         </div> --}}
+
         <div class="grid grid-flow-col grid-rows-3 grid-cols-4">
             <div class="bg-blue-100">1</div>
             <div class="bg-blue-200">2</div>
@@ -28,6 +29,14 @@
             <div class="bg-blue-800">8</div>
             <div class="bg-blue-900">9</div>
         </div>
+
+        <h1 class="">Este es un título de prueba</h1>
+        <p class="">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum nesciunt quod adipisci neque eaque architecto ab a exercitationem deleniti tenetur. Adipisci ea sint, dicta autem ipsa nam corporis in praesentium!</p>
+        <ul>
+            <li class="">Elemento #01</li>
+            <li class="">Elemento #02</li>
+            <li class="">Elemento #03</li>
+        </ul>
     </div>
 </body>
 </html>
